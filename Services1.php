4d28a92af65e26c57e2b0e3a94985793c9a0fbd5
Rashmi Gediya <rashmigediya1999@gmail.com>
2019-02-18 20:01:02 +0530

    test

---
+++ Services1.php
@@ -0,0 +1,33 @@
+
+	<?php include "header.php"; ?>
+
+<!-- start banner Area -->
+<section class="banner-area relative" id="home">	
+				<div class="overlay overlay-bg"></div>
+				<div class="container">				
+					<div class="row d-flex align-items-center justify-content-center">
+						<div class="about-content col-lg-12">
+							<h1 class="text-white">
+								Services				
+							</h1>
+							<br>
+							<input type="text" name="location" placeholder="Enter Location">
+							<input type="text" name="service_type" placeholder="Enter Service Type">
+							<button type="button">SEARCH</button>	
+							<!--<p class="text-white link-nav"><a href="index.html">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="services.html"> Services</a></p>
+					-->	</div>	
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->
+
+			<div>
+				<div class="image" >
+					<img src="img/water-heater-repairing.jpg" alt="" style="height:200px;width:200px;"> 	
+			</div>
+			</div>
+
+
+
+
+	<?php include "footer.php"; ?>
