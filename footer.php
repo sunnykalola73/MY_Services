7f59c7f4ae4e82017f1f92cbee31d036529f25e4
Your Name <siddhrathyadav1999@gmail.com>
2019-02-18 16:36:43 +0530

    header footer created

---
+++ footer.php
@@ -0,0 +1,67 @@
+
+			<!-- start footer Area -->		
+			<footer class="footer-area section-gap"style="height:450px;width:auto;">
+				<div class="container">
+
+					<div class="row">
+						
+							<div class="single-footer-widget">
+								<h6>About Agency</h6>
+								<p>
+									The world has become so fast paced that people don’t want to stand by reading a page of information, they would much rather look at a presentation and understand the message. It has come to a point 
+								</p>
+							</div>
+						
+					
+							<div class="single-footer-widget">
+								<h6>Navigation Links</h6>
+								<div class="row">
+									<table>
+										<tr>
+											<td width="70px"><li><a href="#" style="color:white;">Home</a></li></td>
+											<td width="100px"><li><a href="#" style="color:white;">Feature</a></li></td>
+											<td width="100px"><li><a href="#" style="color:white;">Services</a></li></td>
+											<td><li><a href="#" style="color:white;">Portfolio</a></li></td>
+										</tr>
+										<tr>
+											<td><li><a href="#" style="color:white;">Team</a></li></td>
+											<td><li><a href="#" style="color:white;">Pricing</a></li></td>
+											<td><li><a href="#" style="color:white;">Blog</a></li></td>
+											<td><li><a href="#" style="color:white;">Contact</a></li></td>
+									</table>
+							</div>							
+							</div>
+						</div>				
+					</div>
+
+					<div class="row footer-bottom d-flex justify-content-between align-items-center">
+						<p class="col-lg-8 col-sm-12 footer-text m-0">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved 
+
+</p>
+						<div class="col-lg-4 col-sm-12 footer-social">
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+							
+						</div>
+					</div>
+				</div>
+			</footer>
+			<!-- End footer Area -->	
+
+			<script src="js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="js/vendor/bootstrap.min.js"></script>			
+			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+  			<script src="js/easing.min.js"></script>			
+			<script src="js/hoverIntent.js"></script>
+			<script src="js/superfish.min.js"></script>	
+			<script src="js/mn-accordion.js"></script>
+			<script src="js/jquery.ajaxchimp.min.js"></script>
+			<script src="js/jquery.magnific-popup.min.js"></script>	
+			<script src="js/owl.carousel.min.js"></script>						
+			<script src="js/jquery.nice-select.min.js"></script>	
+    		<script src="js/jquery.circlechart.js"></script>								
+			<script src="js/mail-script.js"></script>	
+			<script src="js/main.js"></script>	
+		</body>
+	</html>
---
+++ header.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="img/nlogo_icon.png">
+    <!-- Author Meta -->
+    <meta name="author" content="Hackthon">
+    <!-- Meta Description -->
+    <meta name="description" content="">
+    <!-- Meta Keyword -->
+    <meta name="keywords" content="">
+    <!-- meta character set -->
+    <meta charset="UTF-8">
+    <!-- Site Title -->
+    <title>Home Services</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+        <!--
+        CSS
+        ============================================= -->
+        <link rel="stylesheet" href="css/linearicons.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/magnific-popup.css">
+        <link rel="stylesheet" href="css/nice-select.css">							
+        <link rel="stylesheet" href="css/animate.min.css">
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>	
+          <header id="header" id="home" style="height:19%;width:auto;">
+          
+            <div class="container main-menu">
+                <div class="row align-items-center justify-content-between d-flex">
+                  <div id="logo">
+                    <a href="index.html"><img src="img/nlogo.png" alt="Logo Here" title="" /></a>
+                  </div>
+                  <nav id="nav-menu-container">
+                    <ul class="nav-menu">
+                      <li class="menu-active"><a href="index.html">Home</a></li>
+                      <li><a href="about.html">About</a></li>
+                      <li><a  class="menu-has-children" href="services.html">Services</a>
+                        <ul>
+                            <li><h1><a href="cleaning_services.html">Home Cleaning</a></h1></li>
+                            <li><a href="cleaning_services.html">Interior & Exterior Designing</a></li>
+                            <li><a href="cleaning_services.html">Appliances Installation & Maintainance</a></li>
+                            <li><a href="cleaning_services.html">Shifting</a></li>
+                            <li><a href="cleaning_services.html">Plumbing</a></li>
+                        </ul>
+                      </li>
+                      <li><a href="feedback.html">Feedback</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                                <?php if($set_uname==0){ ?>
+                      <li><a href="#login">LogIn</a><li>
+                                <?php }else{ ?>
+                      <li><a href="#"><b><?php echo $username; ?></b></a>
+                          <ul>
+                              <li><a href="#">My Services</a></li>
+                              <li><a href="#">Account</a></li>
+                              <li><a href="./do_logout.php">Log Out</a></li>
+                          </ul>
+                                </li>
+                                <?php } ?>
+                    </ul>
+                  </nav><!-- #nav-menu-container -->		    		
+                </div>
+            </div>
+          </header><!-- #header -->
