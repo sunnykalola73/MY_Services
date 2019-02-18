9590288f630b3b9537ec846dedf993c634b0b01e
Parth477 <parthghinaiya477@gmail.com>
2019-02-18 20:26:33 +0530

    123

--- header.php
+++ header.php
@@ -42,13 +42,15 @@
                   <nav id="nav-menu-container">
                     <ul class="nav-menu">
                       <li class="menu-active"><a href="index.php">Home</a></li>
-                      <li><a href="about.html">About</a></li>
+                     
                       <li><a  class="menu-has-children" href="services.html">Services</a>
                        
                       </li>
+                      <li><a href="contact.html">Contact</a></li>
                       <li><a href="feedback.html">Feedback</a></li>
-                                <li><a href="contact.html">Contact</a></li>
+                               
                                 <?php if($set_uname==0){ ?>
+                                  <li><a href="about.html">About</a></li>
                       <li><a href="#login">LogIn</a><li>
                                 <?php }else{ ?>
                       <li><a href="#"><b><?php echo $username; ?></b></a>
